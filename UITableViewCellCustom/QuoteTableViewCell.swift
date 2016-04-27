60dccc8738a48e4cc68b9dc71fc9f177bc7ecd85
Lawrence F MacFadyen <larry.macfadyen@gmail.com>
2016-04-27 14:01:56 -0400

    Initial commit

---
+++ UITableViewCellCustom/QuoteTableViewCell.swift
@@ -0,0 +1,27 @@
+//
+//  QuoteTableViewCell.swift
+//  UITableViewCellCustom
+//
+//  Created by Lawrence F MacFadyen on 2016-04-26.
+//  Copyright © 2016 LawrenceM. All rights reserved.
+//
+
+import UIKit
+
+class QuoteTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var quoteLabel: UILabel!
+    @IBOutlet weak var authorLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
--- UITableViewCellCustom/ViewController.swift
+++ UITableViewCellCustom/ViewController.swift
@@ -8,18 +8,66 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+// Struct for the famous quotes
+struct Quote {
+    var text: String
+    var author: String
+}
+
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+
+    @IBOutlet weak var tableView: UITableView!
+    
+    // Famous quotes and authors
+    let quotes = [Quote(text: "You miss 100% of the shots you don’t take.", author: "Wayne Gretzky"),Quote(text: "If you don’t design your own life plan, chances are you’ll fall into someone else’s plan. And guess what they have planned for you? Not much.", author: "Jim Rohn"),Quote(text: "A goal should scare you a little, and excite you a lot.", author: "Joe Vitale"),Quote(text: "Start by doing what's necessary; then do what's possible; and suddenly you are doing the impossible.", author: "Francis of Assisi")]
+    
+    // Identifier for the table view cell
+    let cellIdentifier = "quoteCell"
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        tableView.delegate = self
+        tableView.dataSource = self
+        
+        // Only show separators where there is data
+        tableView.tableFooterView = UIView(frame: CGRectZero)
+        
+        // Dynamically size the height of the table view cells
+        tableView.estimatedRowHeight = 64.0
+        tableView.rowHeight = UITableViewAutomaticDimension
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
-
+    
+    // Only one section in the table view
+    func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    // Rows is equal to the number of Quotes defined above
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return quotes.count
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        // Our custom cell so we can access the quote text and author
+        let cell = tableView.dequeueReusableCellWithIdentifier(cellIdentifier, forIndexPath: indexPath) as! QuoteTableViewCell
+        
+        // Surrounding quotes with quotation marks and factoring in locale
+        let locale = NSLocale.currentLocale()
+        let qBegin = locale.objectForKey(NSLocaleQuotationBeginDelimiterKey) as? String ?? "\""
+        let qEnd = locale.objectForKey(NSLocaleQuotationEndDelimiterKey) as? String ?? "\""
+        
+        let row = indexPath.row
+        // Set the labels in the custom cell
+        cell.quoteLabel.text = qBegin + quotes[row].text + qEnd
+        cell.authorLabel.text = quotes[row].author
+        
+        return cell
+    }
 
 }
 
